6818cd78bc778161cbb57e3a0601d7a16bb1764a
thejahangir <jahangirxl@gmail.com>
2026-05-25 15:30:42 +0530

    loadEnv added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 // export default defineConfig({
@@ -21,7 +22,10 @@ export default defineConfig(({ mode }) => {
   return {
     base: '/scanjd/',
 
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
 
     server: {
       port: 3000,
