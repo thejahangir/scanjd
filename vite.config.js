32b6e24659ffae31cb21e3246871b7452d7c20ca
thejahangir <jahangirxl@gmail.com>
2026-05-25 15:27:30 +0530

    deploy code issue fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,39 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/scanjd/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+// export default defineConfig({
+//   return {
+//     base: "/scanjd/",
+//     plugins: [
+//       react(),
+//       tailwindcss(),
+//     ],
+
+//   }
+
+// })
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    base: '/scanjd/',
+
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    build: {
+      chunkSizeWarningLimit: 1600  // In kB, adjust as needed
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
+});
+
